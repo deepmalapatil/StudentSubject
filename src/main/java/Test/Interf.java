551a85124dc01e0345f138a2f636e6cd86ef5d52
nirajc1 <81496947+nirajc1@users.noreply.github.com>
2022-12-19 22:15:22 -0500

    lambda expretion with parameter

--- src/main/java/Test/Interf.java
+++ src/main/java/Test/Interf.java
@@ -1,18 +1,23 @@
 package Test;
 
 public interface Interf {
-    public void m1();
+    public void add(int a,int b);
 }
 
-  /*  class Demo implements Interf {
-        public void m1() {
-            System.out.println("hello");
+   /* class Demo implements Interf {
+        public void add(int a,int b) {
+            System.out.println(a+b);
         }
     }*/
 class test1{
     public static void main(String[] args) {
-        Interf i= () -> System.out.println("hello....with lamda expretion");
-        i.m1();
+        /*Interf i= new Demo();
+        i.add(10,20);*/
+
+//lambada expretion
+
+        Interf i= (a,b)-> System.out.println(a+b);
+        i.add(10,20);
     }
 }
 
